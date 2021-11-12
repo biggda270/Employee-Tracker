20594ad2a510148af6ebe6ee4e3677877bade594
Daniel Allen <danielallen1976@gmail.com>
2021-11-12 09:46:02 -0600

    setup

---
+++ server.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+require("console.table")
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "root",
+    database: "daniel1"
+});
+
