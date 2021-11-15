5ff77d01a1fa4ea06c6c047a1dafd7cb7d1b8cc9
Daniel Allen <danielallen1976@gmail.com>
2021-11-14 18:55:23 -0600

    Final

--- server.js
+++ server.js
@@ -6,7 +6,25 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "root",
+    password: "Yellow31782!",
     database: "daniel1"
 });
 
+connection.connect();
+
+connection.connect((err) => {
+    if(err) throw err;
+    console.log('Connected to MySQL Server!');
+});
+
+app.get("/",(req,res) => {
+    connection.query('SELECT * from users LIMIT 1', (err, rows) => {
+        if(err) throw err;
+        console.log('The data from users table are: \n', rows);
+        connection.end();
+    });
+});
+
+app.listen(3306, () => {
+    console.log('Server is running at port 3306');
+});
