bddec1ce85fbdaa30e83223bfd6889b01b11af68
Daniel Allen <danielallen1976@gmail.com>
2021-11-14 18:58:56 -0600

    PW CHANGE
    enter

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Yellow31782!",
+    password: "enter",
     database: "daniel1"
 });
 
@@ -28,3 +28,59 @@ app.get("/",(req,res) => {
 app.listen(3306, () => {
     console.log('Server is running at port 3306');
 });
+
+
+function beginningPrompt() {
+
+    inquirer.prompt({
+        type: "list",
+        name: "task",
+        message: "What would you like to do?",
+        choices: [
+            "View All Employees",
+            "View All Employees by Department",
+            "View All Employees by Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+            "End"]
+
+        })
+        
+        .then(function({ task }) {
+            switch (task) {
+                case "View All Employees":
+                    viewAllEmployees();
+                    break;
+                case "View All Employees by Department":
+                    viewAllEmployeesByDepartment();
+                    break;
+                case "View All Employees by Manager":
+                    viewAllEmployeesByManager();
+                    break;
+                case "Add Employee":
+                    addEmployee();
+                    break;
+                case "Remove Employee":
+                    removeEmployee();
+                    break;
+                case "Update Employee Role":
+                    updateEmployeeRole();
+                    break;
+                case "Update Employee Manager":
+                    updateEmployeeManager();
+                    break;   
+
+                case "End":
+                    connection.end();
+                    break;
+            }
+        }); 
+}
+
+
+
+
+
+        
